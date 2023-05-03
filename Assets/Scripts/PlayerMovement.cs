32a0e39b03a846a840b2f7a0bccdb3a3938fff4e
NightShift3201 <112355963+NightShift3201@users.noreply.github.com>
2023-05-02 22:24:09 -0700

    melee attack (sword + animation)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,27 +8,60 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Animator animator;
+    public Animator swordAnimator;
 
     Vector2 movement;
+
+    public float attackCounter;
+    public float attackSpeed;
+    public bool canMove = true;
+
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if(canMove){
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
 
 
-        animator.SetFloat("Horizontal",movement.x);
-        animator.SetFloat("Vertical",movement.y);
-        animator.SetFloat("Speed",movement.sqrMagnitude);
+            animator.SetFloat("Horizontal",movement.x);
+            animator.SetFloat("Vertical",movement.y);
+            animator.SetFloat("Speed",movement.sqrMagnitude);
 
-        if(movement.x != 0 || movement.y !=0){
-            animator.SetFloat("LastHorizontal",movement.x);
-            animator.SetFloat("LastVertical",movement.y);
+            if(movement.x != 0 || movement.y !=0){
+                animator.SetFloat("LastHorizontal",movement.x);
+                animator.SetFloat("LastVertical",movement.y);
+                swordAnimator.SetFloat("LastHorizontal",movement.x);
+                swordAnimator.SetFloat("LastVertical",movement.y);
+            }
+        }
+    
+
+        if(attackCounter<=0f){
+            if(Input.GetMouseButtonDown(0)){
+                attackCounter = attackSpeed;
+                animator.SetTrigger("Attacking");
+                swordAnimator.SetTrigger("Attacking");
+            }
+
+        }
+        else{
+            attackCounter-=Time.deltaTime;
         }
     }
 
     void FixedUpdate(){
-        rb.MovePosition(rb.position + movement.normalized *moveSpeed*Time.fixedDeltaTime);
+        if(canMove)
+            rb.MovePosition(rb.position + movement.normalized *moveSpeed*Time.fixedDeltaTime);
     }
+
+    public void LockMovement(){
+        canMove=false;
+    }
+
+    public void UnlockMovement(){
+        canMove=true;
+    }
+
 }
